7bb4ad7c7b5e8b2bfd31b09b9c7b42cfbaab31d1
mArker <252133226@qq.com>
2016-12-07 09:05:52 +0800

    编译lib

---
+++ gulpfile.js
@@ -0,0 +1,174 @@
+/**
+ * Copyright (c) 2013-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+'use strict';
+
+var babel = require('gulp-babel');
+var del = require('del');
+var cleanCSS = require('gulp-clean-css');
+var concatCSS = require('gulp-concat-css');
+var derequire = require('gulp-derequire');
+var flatten = require('gulp-flatten');
+var gulp = require('gulp');
+var gulpUtil = require('gulp-util');
+var header = require('gulp-header');
+var packageData = require('./package.json');
+var rename = require('gulp-rename');
+var runSequence = require('run-sequence');
+var through = require('through2');
+var webpackStream = require('webpack-stream');
+
+var fbjsConfigurePreset = require('babel-preset-fbjs/configure');
+var gulpCheckDependencies = require('fbjs-scripts/gulp/check-dependencies');
+
+// var moduleMap = require('./scripts/module-map');
+
+var paths = {
+  dist: 'dist',
+  lib: 'lib',
+  src: [
+    'src/**/*.js',
+    'src/**/*.jsx'
+    // '!src/**/__tests__/**/*.js',
+    // '!src/**/__mocks__/**/*.js',
+  ],
+  css: [
+    'src/**/*.less',
+  ],
+};
+
+var babelOptsJS = {
+  presets: [
+    fbjsConfigurePreset({
+      stripDEV: true,
+      rewriteModules: {},
+    }),
+  ],
+};
+
+var babelOptsFlow = {
+  presets: [
+    fbjsConfigurePreset({
+      target: 'flow',
+      rewriteModules: {},
+    }),
+  ],
+};
+
+var COPYRIGHT_HEADER = `/**
+ * Draft v<%= version %>
+ *
+ * Copyright (c) 2013-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+`;
+
+var buildDist = function(opts) {
+  var webpackOpts = {
+    debug: opts.debug,
+    externals: {
+      immutable: 'Immutable',
+      react: 'React',
+      'react-dom': 'ReactDOM',
+    },
+    output: {
+      filename: opts.output,
+      libraryTarget: 'var',
+      library: 'Draft',
+    },
+    plugins: [
+      new webpackStream.webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify(
+          opts.debug ? 'development' : 'production'
+        ),
+      }),
+      new webpackStream.webpack.optimize.OccurenceOrderPlugin(),
+      new webpackStream.webpack.optimize.DedupePlugin(),
+    ],
+  };
+  if (!opts.debug) {
+    webpackOpts.plugins.push(
+      new webpackStream.webpack.optimize.UglifyJsPlugin({
+        compress: {
+          hoist_vars: true,
+          screw_ie8: true,
+          warnings: false,
+        },
+      })
+    );
+  }
+  return webpackStream(webpackOpts, null, function(err, stats) {
+    if (err) {
+      throw new gulpUtil.PluginError('webpack', err);
+    }
+    if (stats.compilation.errors.length) {
+      gulpUtil.log('webpack', '\n' + stats.toString({colors: true}));
+    }
+  });
+};
+
+gulp.task('clean', function() {
+  return del([paths.dist, paths.lib]);
+});
+
+gulp.task('modules', ['styles'], function() {
+  return gulp
+    .src(paths.src)
+    .pipe(babel(babelOptsJS))
+    .pipe(flatten())
+    .pipe(gulp.dest(paths.lib));
+});
+
+gulp.task('styles', function() {
+  return gulp
+    .src(paths.css)
+    .pipe(flatten())
+    .pipe(gulp.dest(paths.lib));
+});
+
+
+gulp.task('dist', ['modules'], function() {
+  var opts = {
+    debug: true,
+    output: 'Draft.js',
+  };
+  return gulp.src('./lib/Draft.js')
+    .pipe(buildDist(opts))
+    .pipe(derequire())
+    .pipe(header(COPYRIGHT_HEADER, {version: packageData.version}))
+    .pipe(gulp.dest(paths.dist));
+});
+
+gulp.task('dist:min', ['modules'], function() {
+  var opts = {
+    debug: false,
+    output: 'Draft.min.js',
+  };
+  return gulp.src('./lib/Draft.js')
+    .pipe(buildDist(opts))
+    .pipe(header(COPYRIGHT_HEADER, {version: packageData.version}))
+    .pipe(gulp.dest(paths.dist));
+});
+
+
+gulp.task('watch', function() {
+  gulp.watch(paths.src, ['modules']);
+});
+
+gulp.task('dev', function() {
+  gulp.watch(paths.src, ['modules', 'dist']);
+});
+
+gulp.task('default', function(cb) {
+  runSequence('clean', 'modules', ['dist', 'dist:min'], cb);
+});
